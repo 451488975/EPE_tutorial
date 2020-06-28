05960a9bdde45341922399f114a18e5f02f089d7
451488975 <451488975@qq.com>
2020-06-27 18:04:40 -0700

    replace original repo

---
+++ notebooks/OtherFiles/stats.py
@@ -0,0 +1,36 @@
+import statistics
+def mean(vals):
+    """Computes the mean from a list of values."""
+    try:
+        total = float(sum(vals))
+        length = len(vals)
+    except TypeError:
+        raise TypeError("The list was not numbers.")
+    except:
+        print("Something unknown happened with the list.")
+    return total/length
+
+def median(vals):
+    """Computes the median from a list of values."""
+    return statistics.median(vals)
+
+def mode(vals):
+    """Computes the mode from a list of values."""
+    return statistics.mode(vals)
+
+def std(vals):
+    """Computes the standard deviation from a list of values."""
+    n = len(vals)
+    if n == 0:
+        return 0.0
+    mu = sum(vals) / n
+    if mu == 1e500:
+        return NotImplemented
+    var = 0.0
+    for val in vals:
+        var = var + (val - mu)**2
+    return (var / n)**0.5
+
+def var(vals):
+    """Computes the variance from a list of values."""
+    return statistics.variance(vals)
